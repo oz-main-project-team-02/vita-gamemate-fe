05090d44ca9150b323c6f610de27f48dbce5f27b
mi-suni <mi88250819@gmail.com>
2024-10-13 04:06:58 +0900

    EditInfoPage 디자인

--- src/pages/EditInfoPage.tsx
+++ src/pages/EditInfoPage.tsx
@@ -1,8 +1,14 @@
+import FilterList from "../components/EditInfoPage/FilterList";
+import MyPageBar from "../components/EditInfoPage/MyPageBar";
+import ProfileImg from "../components/EditInfoPage/ProfileImg";
+
 export default function EditInfoPage() {
   return (
-    <div>
-      <h1>EditInfoPage</h1>
-      <p>Welcome to the EditInfoPage page</p>
+    <div className="w-full h-[4105px]">
+      {/* <NavBar /> */}
+      <MyPageBar titleE = {"MY PROFILE"} titleK = {"프로필 편집"} content = {"멋진 실력을 자랑해주세요!"} />
+      <ProfileImg />
+      <div className="w-[673px] h-[1866px] absolute top-[233px] flex justify-end bg-[#E2E2E2]"><FilterList /></div>
     </div>
   );
 }

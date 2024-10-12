f48fc75681e229208088f76ad1a7741e27530be1
mi-suni <mi88250819@gmail.com>
2024-10-13 04:07:37 +0900

    OrdersPage 디자인

--- src/pages/OrdersPage.tsx
+++ src/pages/OrdersPage.tsx
@@ -1,8 +1,14 @@
+import FilterList from "../components/EditInfoPage/FilterList";
+import MyPageBar from "../components/EditInfoPage/MyPageBar";
+import ProfileImg from "../components/EditInfoPage/ProfileImg";
+
 export default function OrdersPage() {
   return (
-    <div>
-      <h1>Orders</h1>
-      <p>Welcome to the orders page</p>
+    <div className="w-full h-[4105px]">
+      {/* <NavBar /> */}
+      <MyPageBar titleE = {"MY ORDER"} titleK = {"나의 의뢰"} content = {"즐거운 매칭을 비타와 함께하세요!"} />
+      <ProfileImg />
+      <div className="w-[673px] h-[1866px] absolute top-[233px] flex justify-end bg-[#E2E2E2]"><FilterList /></div>
     </div>
   );
 }

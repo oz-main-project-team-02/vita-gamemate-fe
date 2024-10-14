5efe93704f8d8df4d272dabbc5eadd40eb5368df
shin-minhyuk <afs_style@naver.com>
2024-10-15 03:53:35 +0900

    ✅ feat: MateCard 컴포넌트 추가, CategoryPage UI 구현

--- src/components/Homepage/GameCategorySlider.tsx
+++ src/components/Homepage/GameCategorySlider.tsx
@@ -4,8 +4,7 @@ import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import { GrPrevious } from "react-icons/gr";
 import { GrNext } from "react-icons/gr";
-import OnlineFlag from "../Common/OnlineFlag";
-import VitaPrice from "../Common/VitaPrice";
+import MateCard from "../Common/MateCard";
 
 export default function GameCategorySlider() {
   return (
@@ -24,31 +23,7 @@ export default function GameCategorySlider() {
         {/* 각 슬라이드 */}
         {new Array(12).fill(0).map((_, i) => (
           <SwiperSlide key={i}>
-            <div className='relative flex flex-col items-center justify-center w-full h-[288px] rounded-3xl shadow-lg overflow-hidden leading-[1.3]'>
-              <OnlineFlag />
-              <div className='w-[206px] h-[206px] bg-blue-500'>
-                <img
-                  src='/src/assets/imgs/user.png'
-                  alt=''
-                  className='w-[206px]'
-                />
-              </div>
-              <div className='w-[206px] h-[82px] px-4 py-2'>
-                <h2>닉네임</h2>
-                <p className='flex items-center'>
-                  <img
-                    src='/src/assets/imgs/star.svg'
-                    alt='리뷰 별점 아이콘'
-                    className='w-[18px] h-[18px]'
-                  />
-                  &nbsp;5.00&nbsp;
-                  <span className='text-gray-300 text-sm'>
-                    | 받은 의뢰수 10
-                  </span>
-                </p>
-                <VitaPrice />
-              </div>
-            </div>
+            <MateCard />
           </SwiperSlide>
         ))}
       </Swiper>

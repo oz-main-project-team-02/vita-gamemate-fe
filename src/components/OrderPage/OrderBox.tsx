edda5e6db4b662e748cc7317ccc263fd06d6c44d
mi-suni <mi88250819@gmail.com>
2024-10-17 02:17:06 +0900

    디자인 수정

--- src/components/OrderPage/OrderBox.tsx
+++ src/components/OrderPage/OrderBox.tsx
@@ -1,21 +1,32 @@
+import { useState } from "react";
 import Gender from "../Common/Gender";
+import OnlineFlag from "../Common/OnlineFlag";
+import Review from "./Review";
 
-export default function OrderBox() {
+interface ReviewProps {
+  review: boolean
+}
+
+export default function OrderBox({review}: ReviewProps) {
+  const [showReview, setShowReview] = useState(false)
+
   return (
     <div className="w-full h-[213px] mt-[38px] px-[37px] py-[25px] flex border rounded-xl border-gray-200 bg-[#FFFFFF]">
       <div className="w-[163px] h-[163px] border rounded-3xl border-gray-200 bg-[#F8F8F8]">
-        <img className="" src="../../../public/vitaLogo.png" alt="vitaLogo" />
+        <img className="w-[160px] h-[160px] p-1" src="/src/assets/imgs/user.png" alt="user" />
       </div>
       <div className="w-1/2 h-[163px] mx-8 py-2">
         <p className="text-base">닉네임</p>
-        <div className="w-[114px] h-[19px] mt-1 mb-[42px] flex justify-between">
+        <div className="w-[142px] h-[19px] mt-1 mb-[42px] flex justify-between">
           <Gender gender={'male'} age={26} />
-          <div className="w-[59px] h-[19px] px-2 flex items-center rounded-xl bg-mintGreen text-[#FFFFFF] font-medium text-xs">•{" "}온라인</div>
+          <div className="w-[100px] mt-[-9px] relative"><OnlineFlag /></div>
         </div>
         <p className="mb-1">의뢰 신청 : 2024 - 10 - 11 - 11시 22분</p>
         <p>총 개수 : 1</p>
       </div>
-      <button className="w-[15%] h-[50px] mt-[50px] ml-[50px] rounded-xl bg-gradient-to-r from-primary to-limeGreen font-semibold text-[20px]">리뷰쓰기</button>
+      {review && <button onClick={() => setShowReview(true)} 
+      className="w-[15%] h-[50px] mt-[50px] ml-[50px] rounded-xl bg-gradient-to-r from-primary to-limeGreen font-semibold text-[20px]">리뷰쓰기</button>}
+      {showReview && <Review setShowReview={setShowReview} />}
     </div>
   )
 }

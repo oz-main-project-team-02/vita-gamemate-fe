e5806470bec0026cfea302ee38f413c2aa3b01a1
mi-suni <mi88250819@gmail.com>
2024-10-18 15:43:41 +0900

    design: ErrorPage 스타일 구현

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,22 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./global.css";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './global.css';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 async function enableMocking() {
-  if (import.meta.env.VITE_NODE_ENV !== "development") {
+  if (import.meta.env.VITE_NODE_ENV !== 'development') {
     return;
   }
 
-  const { worker } = await import("./mock/browser");
-  return worker.start({ onUnhandledRequest: "bypass" });
+  const { worker } = await import('./mock/browser');
+  return worker.start({ onUnhandledRequest: 'bypass' });
 }
 
 const queryClient = new QueryClient();
 
 enableMocking().then(() => {
-  createRoot(document.getElementById("root")!).render(
+  createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />

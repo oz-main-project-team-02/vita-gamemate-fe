b06da23fd3208fac3fb48887ba64dd9ae893c991
mi-suni <mi88250819@gmail.com>
2024-10-28 15:30:01 +0900

    feat: search 기능 완성 및 UserDetailPage email -> id로 변경

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,11 +1,5 @@
 import { client } from './client';
 
-export const fetchSearchUser = async (search: string) => {
-  try {
-    const { data } = await client.get(`/api/v1/mates/search/?page=1&search=${search}`);
-    console.log(search);
-    console.log(data);
-  } catch (error) {
-    console.error(error.response.data);
-  }
+export const fetchSearchUser = (search: string) => {
+  return client.get(`/api/v1/mates/search/?search=${search}`);
 };

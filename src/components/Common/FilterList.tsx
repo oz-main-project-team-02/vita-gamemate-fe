bf7779a35048cbf48100beb922216d12800be868
shin-minhyuk <afs_style@naver.com>
2024-10-28 13:37:06 +0900

    ✅ design: 프로필 편집 페이지 반응형 수정

--- src/components/Common/FilterList.tsx
+++ src/components/Common/FilterList.tsx
@@ -30,37 +30,39 @@ export default function FilterList() {
   };
 
   return (
-    <div className='mr-[80px] flex h-[400px] w-1/2 flex-col justify-end'>
-      <p className={getNavigateClass('/user/edit-info')} onClick={() => navigate('/user/edit-info')}>
-        <big>
-          <strong>/</strong>
-        </big>{' '}
-        &nbsp;프로필
-      </p>
-      <p className={getNavigateClass('/user/gamemate')} onClick={() => navigate('/user/gamemate')}>
-        <big>
-          <strong>/</strong>
-        </big>{' '}
-        &nbsp;게임 메이트 등록
-      </p>
-      <p className={getNavigateClass('/user/coin')} onClick={() => navigate('/user/coin')}>
-        <big>
-          <strong>/</strong>
-        </big>{' '}
-        &nbsp;코인
-      </p>
-      <p className={getNavigateClass('/user/orders')} onClick={() => navigate('/user/orders')}>
-        <big>
-          <strong>/</strong>
-        </big>{' '}
-        &nbsp;내 의뢰
-      </p>
-      <br />
-      <hr className='h-[1px] border-0 bg-[#898989]' />
-      <br />
-      <p onClick={handleLogoutClick} className='mt-3 cursor-pointer text-[#898989] hover:text-[#3A3A3A]'>
-        로그아웃
-      </p>
+    <div className='flex w-[300px] flex-col items-end bg-[#e2e2e2] p-10 lg:w-[350px] xl:w-[400px]'>
+      <div className='w-2/3'>
+        <p className={getNavigateClass('/user/edit-info')} onClick={() => navigate('/user/edit-info')}>
+          <big>
+            <strong>/</strong>
+          </big>{' '}
+          &nbsp;프로필
+        </p>
+        <p className={getNavigateClass('/user/gamemate')} onClick={() => navigate('/user/gamemate')}>
+          <big>
+            <strong>/</strong>
+          </big>{' '}
+          &nbsp;게임 메이트 등록
+        </p>
+        <p className={getNavigateClass('/user/coin')} onClick={() => navigate('/user/coin')}>
+          <big>
+            <strong>/</strong>
+          </big>{' '}
+          &nbsp;코인
+        </p>
+        <p className={getNavigateClass('/user/orders')} onClick={() => navigate('/user/orders')}>
+          <big>
+            <strong>/</strong>
+          </big>{' '}
+          &nbsp;내 의뢰
+        </p>
+        <br />
+        <hr className='h-[1px] border-0 bg-[#898989]' />
+        <br />
+        <p onClick={handleLogoutClick} className='mt-3 cursor-pointer text-[#898989] hover:text-[#3A3A3A]'>
+          로그아웃
+        </p>
+      </div>
     </div>
   );
 }
--- src/pages/user/CoinPage.tsx
+++ src/pages/user/CoinPage.tsx
@@ -34,67 +34,62 @@ export default function CoinPage() {
 
   return (
     <>
-      <div className='w-full'>
-        <div className='relative'>
-          <TitleIntro titleE={'MY WALLET'} titleK={'나의 코인'} content={'소중한 시간을 비타와 함께하세요!'} />
-        </div>
-        {/* 리팩토링 */}
-        <div className='flex w-full'>
-          {/* 필터 섹션 */}
-          <div className='flex w-[300px] flex-col items-end bg-[#e2e2e2] p-10 lg:w-[350px] xl:w-[400px]'>
-            <div className='w-2/3'>
-              <p className={getNavigateClass('/user/edit-info')} onClick={() => navigate('/user/edit-info')}>
-                <big>
-                  <strong>/</strong>
-                </big>{' '}
-                &nbsp;프로필
-              </p>
-              <p className={getNavigateClass('/user/gamemate')} onClick={() => navigate('/user/gamemate')}>
-                <big>
-                  <strong>/</strong>
-                </big>{' '}
-                &nbsp;게임 메이트 등록
-              </p>
-              <p className={getNavigateClass('/user/coin')} onClick={() => navigate('/user/coin')}>
-                <big>
-                  <strong>/</strong>
-                </big>{' '}
-                &nbsp;코인
-              </p>
-              <p className={getNavigateClass('/user/orders')} onClick={() => navigate('/user/orders')}>
-                <big>
-                  <strong>/</strong>
-                </big>{' '}
-                &nbsp;내 의뢰
-              </p>
-              <br />
-              <hr className='h-[1px] border-0 bg-[#898989]' />
-              <br />
-              <p onClick={handleLogoutClick} className='mt-3 cursor-pointer text-[#898989] hover:text-[#3A3A3A]'>
-                로그아웃
-              </p>
-            </div>
+      <TitleIntro titleE={'MY WALLET'} titleK={'나의 코인'} content={'소중한 시간을 비타와 함께하세요!'} />
+      <div className='flex w-full'>
+        {/* 필터 섹션 */}
+        <div className='flex w-[300px] flex-col items-end bg-[#e2e2e2] p-10 lg:w-[350px] xl:w-[400px]'>
+          <div className='w-2/3'>
+            <p className={getNavigateClass('/user/edit-info')} onClick={() => navigate('/user/edit-info')}>
+              <big>
+                <strong>/</strong>
+              </big>{' '}
+              &nbsp;프로필
+            </p>
+            <p className={getNavigateClass('/user/gamemate')} onClick={() => navigate('/user/gamemate')}>
+              <big>
+                <strong>/</strong>
+              </big>{' '}
+              &nbsp;게임 메이트 등록
+            </p>
+            <p className={getNavigateClass('/user/coin')} onClick={() => navigate('/user/coin')}>
+              <big>
+                <strong>/</strong>
+              </big>{' '}
+              &nbsp;코인
+            </p>
+            <p className={getNavigateClass('/user/orders')} onClick={() => navigate('/user/orders')}>
+              <big>
+                <strong>/</strong>
+              </big>{' '}
+              &nbsp;내 의뢰
+            </p>
+            <br />
+            <hr className='h-[1px] border-0 bg-[#898989]' />
+            <br />
+            <p onClick={handleLogoutClick} className='mt-3 cursor-pointer text-[#898989] hover:text-[#3A3A3A]'>
+              로그아웃
+            </p>
           </div>
+        </div>
 
-          {/* 메인 섹션 */}
-          <div className='relative flex-grow bg-gray-100 p-20'>
-            <ProfileImg />
-            {/* 이벤트 배너 */}
-            <div className='mt-[140px]'>
-              <button className='mb-[55px] h-[90px] w-full cursor-pointer rounded-xl bg-[#FF7A7A] text-3xl text-[#FFFFFF] lg:h-[110px] lg:text-4xl xl:h-[130px] xl:text-5xl'>
-                첫 결제 혜택
-              </button>
+        {/* 메인 섹션 */}
+        <div className='relative flex-grow bg-gray-100 p-20'>
+          <ProfileImg />
+          {/* 이벤트 배너 */}
+          <div className='mt-[140px]'>
+            <button className='mb-[55px] h-[90px] w-full cursor-pointer rounded-xl bg-[#FF7A7A] text-3xl text-[#FFFFFF] lg:h-[110px] lg:text-4xl xl:h-[130px] xl:text-5xl'>
+              첫 결제 혜택
+            </button>
 
-              <div
-                className='grid w-full justify-center gap-4'
-                style={{
-                  gridTemplateColumns: 'repeat(auto-fit, 200px)',
-                }}
-              >
-                {CoinPackages.map((coinData) => (
-                  <CoinBox key={coinData.coin} coinData={coinData} />
-                ))}
-              </div>
+            <div
+              className='grid w-full justify-center gap-4'
+              style={{
+                gridTemplateColumns: 'repeat(auto-fit, 200px)',
+              }}
+            >
+              {CoinPackages.map((coinData) => (
+                <CoinBox key={coinData.coin} coinData={coinData} />
+              ))}
             </div>
           </div>
         </div>

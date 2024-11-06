cfa5eaea99850f381a4c8c53c968c1286884703c
shin-minhyuk <afs_style@naver.com>
2024-11-06 11:57:48 +0900

    🐛 fix: 게임메이트 등록했을 때 메인으로 리다이렉트 돌아가기 불가능, 등록된 게임의 경우 선택하지 못하도록 수정

--- src/components/GameMatePage/GameSection.tsx
+++ src/components/GameMatePage/GameSection.tsx
@@ -1,4 +1,5 @@
 import { Games } from '@/config/const';
+import { useUserStore } from '@/config/store';
 import { MateRegister } from '@/config/types';
 
 type Props = {
@@ -9,6 +10,13 @@ type Props = {
 };
 
 export default function GameSection({ isDropdownOpen, setIsDropdownOpen, game_id, setFormData }: Props) {
+  // fix: possibleGmaeIds = 선택한 게임 필터링 후 반환되는 값
+  const user = useUserStore((state) => state.user);
+
+  const possibleGameIds = Object.values(Games).filter((game) => {
+    return !user.mate_game_info?.some((info) => info.game_id === game.id);
+  });
+
   return (
     <>
       <button
@@ -21,26 +29,27 @@ export default function GameSection({ isDropdownOpen, setIsDropdownOpen, game_id
       {isDropdownOpen.game && (
         <div className='mt-4 rounded-xl bg-white p-4 outline outline-gray-200'>
           <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-            {Object.values(Games).map((game, i) => (
-              <div
-                key={i}
-                onClick={() => setFormData((prev) => ({ ...prev, game_id: game.title }))}
-                className={`flex flex-1 flex-col items-center rounded-xl py-2 hover:scale-110 hover:bg-[#FFD80077] ${game_id === game.title ? 'bg-primary' : null}`}
-              >
-                <div className='rounded-full bg-gray-200 p-4'>
-                  <img
-                    src={game.icon}
-                    alt={`${game.title} 로고`}
-                    className='h-8 min-h-8 w-8 min-w-8 lg:h-12 lg:w-12 xl:h-16 xl:w-16'
-                  />
-                </div>
-                <span
-                  className={`// mt-2 text-center text-xs lg:text-sm xl:text-base ${game_id === game.title ? 'font-semibold' : null} `}
+            {possibleGameIds &&
+              possibleGameIds.map((game, i) => (
+                <div
+                  key={i}
+                  onClick={() => setFormData((prev) => ({ ...prev, game_id: game.title }))}
+                  className={`flex flex-1 flex-col items-center rounded-xl py-2 hover:scale-110 hover:bg-[#FFD80077] ${game_id === game.title ? 'bg-primary' : null}`}
                 >
-                  {game.title}
-                </span>
-              </div>
-            ))}
+                  <div className='rounded-full bg-gray-200 p-4'>
+                    <img
+                      src={game.icon}
+                      alt={`${game.title} 로고`}
+                      className='h-8 min-h-8 w-8 min-w-8 lg:h-12 lg:w-12 xl:h-16 xl:w-16'
+                    />
+                  </div>
+                  <span
+                    className={`// mt-2 text-center text-xs lg:text-sm xl:text-base ${game_id === game.title ? 'font-semibold' : null} `}
+                  >
+                    {game.title}
+                  </span>
+                </div>
+              ))}
           </div>
         </div>
       )}
